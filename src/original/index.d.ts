46dbb79cd55397a7ce2ebc7d0be2adbdd98610c5
Voltstriker <me@voltstriker.dev>
2026-07-02 01:52:51 +1000

    style: add line breaks deleted in previous commit

--- src/original/index.d.ts
+++ src/original/index.d.ts
@@ -35,7 +35,9 @@
 
 declare namespace mod {
     export const stringkeys: Any;
+
     export const strings: Any;
+
     export function Wait(n: number): Promise<void>;
     // Sets the value of a Variable.
     export function SetVariable(variable: Variable, value: Any): void;
